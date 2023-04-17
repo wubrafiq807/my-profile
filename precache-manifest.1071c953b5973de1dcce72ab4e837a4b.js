a5b678cd1696a8c477b1cc8d3ba2947908cbe7d7
Md Rafiqul Islam <wubrafiqcse807@gmail.com>
2023-04-17 19:52:34 +0900

    Updates

---
+++ precache-manifest.1071c953b5973de1dcce72ab4e837a4b.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9813a41c7256473ff7654f2fc1467dd",
+    "url": "/my-profile/index.html"
+  },
+  {
+    "revision": "58352f78da5df551733f",
+    "url": "/my-profile/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "9062399a798b3578c064",
+    "url": "/my-profile/static/css/main.aadfa851.chunk.css"
+  },
+  {
+    "revision": "58352f78da5df551733f",
+    "url": "/my-profile/static/js/2.283d90a5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-profile/static/js/2.283d90a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9062399a798b3578c064",
+    "url": "/my-profile/static/js/main.8f145655.chunk.js"
+  },
+  {
+    "revision": "06018bee6432144686de",
+    "url": "/my-profile/static/js/runtime-main.4dbf26e3.js"
+  },
+  {
+    "revision": "cf5fc5b2dd1cedfab0632e5a55f0b20f",
+    "url": "/my-profile/static/media/3si.cf5fc5b2.png"
+  },
+  {
+    "revision": "a0b063dc8b3ccba4a288e72e7945dced",
+    "url": "/my-profile/static/media/connective.a0b063dc.jpg"
+  },
+  {
+    "revision": "9eb615f998c3de457168a125924bd7e8",
+    "url": "/my-profile/static/media/donkihote.9eb615f9.png"
+  },
+  {
+    "revision": "0273a537211f65ca06cca42c99c1854f",
+    "url": "/my-profile/static/media/email.0273a537.svg"
+  },
+  {
+    "revision": "5d15d75e7b3ad1d25766f3f79a424bf1",
+    "url": "/my-profile/static/media/fryer.5d15d75e.jpg"
+  },
+  {
+    "revision": "3f49cd08fe1bbfb86ea75a0cdb911ba7",
+    "url": "/my-profile/static/media/github.3f49cd08.svg"
+  },
+  {
+    "revision": "461a64012e89d7f6b4b973c1a5d4d5f5",
+    "url": "/my-profile/static/media/hrm.461a6401.png"
+  },
+  {
+    "revision": "9b1d703781347269f0479d91e112930b",
+    "url": "/my-profile/static/media/istvn.9b1d7037.jpg"
+  },
+  {
+    "revision": "ed4cbbe3aa172743980778b64dd8803d",
+    "url": "/my-profile/static/media/linkedin.ed4cbbe3.svg"
+  },
+  {
+    "revision": "8880d260754a039452eadb53681b1589",
+    "url": "/my-profile/static/media/payrole.8880d260.jpg"
+  },
+  {
+    "revision": "6d949d5afabf3f949ff6455f55eb2d9d",
+    "url": "/my-profile/static/media/profile.6d949d5a.jpg"
+  },
+  {
+    "revision": "9af1116ea160c3afa6bfc9bb66f9d2da",
+    "url": "/my-profile/static/media/smartscooter.9af1116e.jpg"
+  },
+  {
+    "revision": "8f58dcba8672dea99490b68d5a541924",
+    "url": "/my-profile/static/media/synergyInc.8f58dcba.png"
+  },
+  {
+    "revision": "4ba2da1a7ead412486355f399c2e12e1",
+    "url": "/my-profile/static/media/twitter.4ba2da1a.svg"
+  },
+  {
+    "revision": "476e2c0dd5c18578c22551b19d831db8",
+    "url": "/my-profile/static/media/voiceRecognition.476e2c0d.jpg"
+  },
+  {
+    "revision": "d21b5ad3892c53e79bd692ff9311faa0",
+    "url": "/my-profile/static/media/weeklyreport.d21b5ad3.png"
+  },
+  {
+    "revision": "315f2c346efb30e642cc6c949db089f7",
+    "url": "/my-profile/static/media/whitebox-2.315f2c34.png"
+  }
+]);
